083be506479575686121705930322ec26ddd7e35
Ashwin-Rajesh <40654785+Ashwin-Rajesh@users.noreply.github.com>
2019-07-19 20:25:28 +0530

    Added documentation

--- UF_QuickUnion.java
+++ UF_QuickUnion.java
@@ -1,23 +1,54 @@
 public class QuickUnion
 {
+	/*
+	About QuickUnion
+	-----------------------------------------------
+	The array id[] stores a number for each point. The
+	points are represented by numbers from 0, and this
+	number can be called their 'index'. The structure 
+	of Quick Union is such that the value stored in id[]
+	of an index of a point will be of points immediately
+	connected to it.
+	
+	constructor():	Initialises id[]
+	All points are initialised with id[] equal to their
+	index. This takes *linear time*
+	
+	connected(): 	Function checks if two points of given indices are connected
+	The structure of Quick Union is such that the id[]
+	stores index of its immediate neighbour. So, to
+	check connection, we need to find the 'root' of the
+	points, ie, the point whose id[] is equal to its index,
+	and compare their roots.
+	
+	union(): 	Function connects two points whose indices are given
+	To connect two points together, we change the id[]
+	of one point to the index of the other. This takes
+	*constant time*.
+	-----------------------------------------------
+	*/
 	private int[] id;
+	
 	public QuickUnion(int N)
 	{
 		id = new int[N];
 		for (int i = 0; i < N; i++)
 			id[i] = i;
 	}
+	
 	public int root(int i)
 	{
 		while( id[i] != i) i = id[i];
 		return i;
 	}
+	
 	public void union(int p, int q)
 	{
 		int i = root(p);
 		int j = root(q);
 		id[i] = j;
 	}
+	
 	public void find(int p, int q)
 	{
 		return root(p) == root(q);
