76dbe835652a679d33d373e60d6c6e15dab7a0bc
Ashwin-Rajesh <40654785+Ashwin-Rajesh@users.noreply.github.com>
2019-07-19 20:03:48 +0530

    Initial commit

---
+++ UF_QuickFind.java
@@ -0,0 +1,21 @@
+public class QuickFind
+{
+	private int[] id;
+	public QuickFind(int N)
+	{
+		id = new int[N];
+		for(int i = 0; i < N; i++)
+			id[i] = i;
+	}
+	public void union(int p, int q)
+	{
+		int p_id = id[p];
+		int q_id = id[q];
+		for(int i = 0; i < id.length(); i++)
+			if(id[i] == p_id) id[i] = q_id;
+	}
+	public boolean connected(int p, int q)
+	{
+		return id[p] == id[q];
+	}
+}
