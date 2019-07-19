36763d55710a2f7d09752dad3fc3bc3a68cdd083
Ashwin-Rajesh <40654785+Ashwin-Rajesh@users.noreply.github.com>
2019-07-19 20:04:48 +0530

    Initial commit

---
+++ UF_QuickUnion.java
@@ -0,0 +1,25 @@
+public class QuickUnion
+{
+	private int[] id;
+	public QuickUnion(int N)
+	{
+		id = new int[N];
+		for (int i = 0; i < N; i++)
+			id[i] = i;
+	}
+	public int root(int i)
+	{
+		while( id[i] != i) i = id[i];
+		return i;
+	}
+	public void union(int p, int q)
+	{
+		int i = root(p);
+		int j = root(q);
+		id[i] = j;
+	}
+	public void find(int p, int q)
+	{
+		return root(p) == root(q);
+	}
+}
