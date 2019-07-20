b9d3de398bcd2216fafe579444b52b305202a24c
Ashwin-Rajesh <40654785+Ashwin-Rajesh@users.noreply.github.com>
2019-07-20 19:38:22 +0530

    Removed unnecessary documentation

--- UF_WeightedQuickUnion.java
+++ UF_WeightedQuickUnion.java
@@ -10,30 +10,8 @@ public class WeightedQuickUnion
 	trees in track. This will be referred when the size is
 	checked while connecting elements together. Smaller trees
 	are connected onto the root of larger ones. This can help
-	keep the tree flat.
-
-	constructor():	Initialises id[]
-	All points are initialised with id[] equal to their
-	index. This takes *linear time*
-	
-	connected(): 	Function checks if two points of given indices are connected
-	The structure of Quick Union is such that the id[]
-	stores index of its immediate neighbour. So, to
-	check connection, we need to find the 'root' of the
-	points, ie, the point whose id[] is equal to its index,
-	and compare their roots. Takes *linearithmetic time*.
-	
-	union(): 	Function connects two points whose indices are given
-	To connect two points together, we change the id[]
-	of root of one point to the index of the root of 
-	the other. The size[] array is referred to ensure that
-	smaller arrays are connected onto larger ones to reduce
-	steps needed to compute root. This takes *linearithmetic time*.
-	
-	root():		Calculates the 'root' of the point whose index is given
-	Is a recurssive function that returns the index
-	of point whose id[] is equal to its index. Takes
-	*linearithmetic time*. Better than just QuickUnion.
+	keep the tree flat, improving time complexity without any 
+	side-effects.
 	-----------------------------------------------
 	*/
   
@@ -52,8 +30,9 @@ public class WeightedQuickUnion
 		}
 	}
 	
-	public int root(int i)
+	public int root(int p)
 	{
+		int i = p;
 		while( id[i] != i)
 		{
 			i = id[i];
