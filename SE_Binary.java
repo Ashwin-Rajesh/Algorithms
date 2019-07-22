b7a538fdb99bbf01e62dca8568a656481d22b7e2
Ashwin-Rajesh <40654785+Ashwin-Rajesh@users.noreply.github.com>
2019-07-22 17:22:44 +0530

    Improved documentation, fixed errors

--- SE_Binary.java
+++ SE_Binary.java
@@ -1,13 +1,20 @@
+/****************************************************
+		Binary Search
+-----------------------------------------------------
+	Binary Search is a search algorithm. It works
+only for pre-sorted arrays only. It works by splitting
+a sorted array in half, and checking on which side of
+the array the search term is likely to be. If the search
+term is on the left side, it avoids the righ side and
+searches the left side. If it is on the right side, it does
+so on that side. The element is found by searching at the
+middle of the search range.
+	(Here, the middle, high and low values are all
+searched. It is implemented as a recursive function.)
+****************************************************/
+
 public class BinarySearch
-{
-	/*
-	BinarySearch
-	---------------------------------------------------------------------------------------------------------------------------
-	Binary search is a searching algorithm, that is logarithmic(base 2). It works only for sorted arrays. It works by comparing middle element of the array with the search item. If search item is supposed to go before the middle element, it considers the first half of the array and repeats the same process on that part. If the search item is supposed to go after the search element, it considers the second half. It repeats this untill either the middle element corresponds with the search element, or the part of array considered by the sorting algorithm vanishes, ie when the part of array to be considered has just one element and it isnt equal to search item.
-	---------------------------------------------------------------------------------------------------------------------------
-	*/
-	public BinarySearch() {}
-	
+{	
 	public static int search(int[] array, int p)
 	{
 		return search(array,0,array.length - 1,p);
@@ -18,6 +25,10 @@ public class BinarySearch
 		int mid = low + (high - low)/2;
 		if(array[mid] == p)
 			return mid;
+		if(array[low] == p)
+			return low;
+		if(array[high] == p)
+			return high;
 		if(low == high)
 			return -1;
 		if(p > array[mid])
