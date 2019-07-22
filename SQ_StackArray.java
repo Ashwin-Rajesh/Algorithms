81635444edbfd5c066b044f96585a9fafeffae41
Ashwin-Rajesh <40654785+Ashwin-Rajesh@users.noreply.github.com>
2019-07-22 19:45:04 +0530

    Fixed errors

--- SQ_StackArray.java
+++ SQ_StackArray.java
@@ -1,27 +1,27 @@
+/*************************************************
+		Stack as Array
+--------------------------------------------------
+	Stack implements the LIFO(Last In First Out)
+concept. The element that is entered or pushed into
+the stack the last is the one to be retrieved the first.
+
+	here, it is implemented using a non-resizing array.
+*************************************************/
 public class StackArray <Item>
 {
-	/*
-	Stack as an Array
-	------------------------------------------------
-	Stack implements the LIFO(Last In First Out) concept.
-	The last entered element is retreived first.
-	
-	Here, stack is implemented as an array.
-	------------------------------------------------
-	*/
 	private Item[] stack;
 	
 	private int size;
 	
 	public StackArray(int n)
 	{
-		stack = new Item[n];
+		stack = (Item[])new Object[n];
 		size = 0;
 	}
 	
 	public void push(Item item)
 	{
-		if(size >= stack.length())		throw new java.lang.IndexOutOfBoundsException("The stack is full");
+		if(size >= stack.length)		throw new java.lang.IndexOutOfBoundsException("The stack is full");
 		else
 		
 		stack[size] = item;
