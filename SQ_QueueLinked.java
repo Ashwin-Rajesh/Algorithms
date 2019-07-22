bdcce796f9f3292ea7817904349979d3854d3443
Ashwin-Rajesh <40654785+Ashwin-Rajesh@users.noreply.github.com>
2019-07-22 23:16:31 +0530

    Create SQ_QueueLinked.java

---
+++ SQ_QueueLinked.java
@@ -0,0 +1,62 @@
+/********************************************************
+		Queue as Linked List
+---------------------------------------------------------
+	Queue implements the FIFO(First In First Out) concept.
+The first value to be entered is the one that will be retrieved
+the first.
+	Here, it is implemented as a linked list. The class
+maintains two nodes of the linked list, one of the end and one
+of the beginning. The nodes are added to the end and removed
+from the beginning.
+********************************************************/
+
+public class QueueLinkedList <Item>
+{
+	private class Node
+	{
+		public Item item;
+		
+		public Node next;
+	}
+	
+	private Node end;
+	
+	private Node beg;
+	
+	public QueueLinkedList()
+	{
+		end = null;
+		beg = null;
+	}
+	
+	public void push(Item item)
+	{
+		Node temp = new Node();
+		temp.item = item;
+		temp.next = null;
+		if(end == null)
+		{
+			end = temp;
+			beg = temp;
+		}
+		else
+		{
+			end.next = temp;
+			end = temp;
+		}
+	}
+	
+	public Item pop()
+	{
+		if(beg == null)				throw new java.util.NoSuchElementException();
+		Node temp = beg;
+		beg = beg.next;
+		return temp.item;
+	}
+	
+	public boolean IsEmpty()
+	{
+		if(beg == null)	return true;
+		return false;
+	}
+}
