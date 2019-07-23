347792a2728b7d21af607bf2d0a5ee92e477ed3b
Ashwin-Rajesh <40654785+Ashwin-Rajesh@users.noreply.github.com>
2019-07-23 19:07:08 +0530

    Changed name of class

--- SQ_StackArray.java
+++ SQ_StackArray.java
@@ -7,13 +7,14 @@ the stack the last is the one to be retrieved the first.
 
 	here, it is implemented using a non-resizing array.
 *************************************************/
-public class StackArray <Item>
+
+public class SQ_StackArray <Item>
 {
 	private Item[] stack;
 	
 	private int size;
 	
-	public StackArray(int n)
+	public SQ_StackArray(int n)
 	{
 		stack = (Item[])new Object[n];
 		size = 0;
